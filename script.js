1dbd39aac67f9dc38a59c15341e476a414e37f0a
sree <sree@Ravi-MacBook-Air-M1.local>
2024-03-04 22:21:56 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const express =require("express")
-const app = express()
+const express = require("express");
+const path = require("path");
+const cors =require("cors")
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const userData = [
+  { id: 1, name: "sree", compamy: "wac" },
+  { id: 2, name: "ravi", compamy: "ust" },
+  { id: 3, name: "sreeja", compamy: "airindia" },
+];
+
+app.get("/login", (req, res) => {
+  res.json(userData);
+});
 
 
-app.get("/" ,(req,res)=>{
-    res.send("home")
+app.post("/addUser" ,(req , res )=>{
+    const data =req.body
+    console.log(data)
 })
 
-app.listen(8000)
-console.log("dev")
+app.get("*", (req, res) => {
+    res.json("NO Route");
+  });
+
+app.listen(8000);
